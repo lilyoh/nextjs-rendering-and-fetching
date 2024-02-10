8e59cbc6b4a6f6df6d13633a4a4b34d16e955234
lilyoh <lilyoh628@gmail.com>
2024-02-10 16:56:18 +1100

    get to know about getStaticsProps()

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,25 @@ export async function getStaticProps() {
 	const jsonData = await fs.readFile(filePath);
 	const data = JSON.parse(jsonData);
 
+	if (!data) {
+		return {
+			redirect: {
+				destination: '/no-data',
+			},
+		};
+	}
+
+	if (data.products.length === 0) {
+		return {
+			notFound: true, // use when data fetching failed
+		};
+	}
+
 	return {
 		props: {
 			products: data.products,
 		},
+		revalidate: 10,
 	};
 }
 
