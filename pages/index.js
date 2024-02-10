6973450a133c8182831da6c29924add22840f936
lilyoh <lilyoh628@gmail.com>
2024-02-10 16:20:51 +1100

    use server side code and file system

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import path from 'path';
+import fs from 'fs/promises';
+
 function HomePage(props) {
 	const { products } = props;
 
@@ -14,9 +17,13 @@ function HomePage(props) {
 // 1. next executes this func
 // 2. and then executes component func
 export async function getStaticProps() {
+	const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+	const jsonData = await fs.readFile(filePath);
+	const data = JSON.parse(jsonData);
+
 	return {
 		props: {
-			products: [{ id: 'p1', title: 'Product 1' }],
+			products: data.products,
 		},
 	};
 }
