9a0c8f67cadba75f7c98262d9a145afe4a600a56
lilyoh <lilyoh628@gmail.com>
2024-02-14 21:16:55 +1100

    add getServerSideProps

---
+++ pages/user-profile.js
@@ -0,0 +1,11 @@
+function UserProfilePage(props) {
+	return <h1>{props.username}</h1>;
+}
+
+export default UserProfilePage;
+
+export async function getServerSideProps(context) {
+	return {
+		props: { username: 'Max' },
+	};
+}
